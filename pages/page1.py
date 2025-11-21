73002299169b1ecc3232ad54e3516cecee155173
chyntiai <chyntialokananta@gmail.com>
2025-11-21 19:24:50 +0700

    menambahkan file page1, page2, page3.

---
+++ pages/page1.py
@@ -0,0 +1,9 @@
+"""
+Exampe from
+https://msaqib-genai.medium.com/how-to-create-a-multi-page-app-in-streamlit-7f788e715db4
+"""
+
+import streamlit as st
+
+st.title("welcome to the chyntia's  homepage)
+st.write("ini demo aplikasi streamlit.")
---
+++ pages/page3.py
@@ -0,0 +1,4 @@
+import stremlit as st
+
+st.title("settings")
+st.write("this page allows you to cutomize your preference.")
